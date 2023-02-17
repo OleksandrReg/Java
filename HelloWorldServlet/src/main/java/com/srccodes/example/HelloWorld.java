636047e0e9d044136d1fc652d9bd73ba6450deb3
Oleksandr <regsasha9&@gmail.com>
2023-02-17 17:10:52 +0200

    Adds

--- HelloWorldServlet/src/main/java/com/srccodes/example/HelloWorld.java
+++ HelloWorldServlet/src/main/java/com/srccodes/example/HelloWorld.java
@@ -15,31 +15,26 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
       
-     * @see HttpServlet#HttpServlet()
     public HelloWorld() {
         super();
     }
 
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>Hello World!</h1>");
 	}
 
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>Hello World!</h1>");
 	}
 	
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse) 
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>Hello World!</h1>");
 	}
 	
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>Hello World!</h1>");
