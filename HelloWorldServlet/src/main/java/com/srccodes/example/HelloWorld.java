57686d7d856f9fd8494d10250b729a990cd3c14c
Oleksandr <regsasha9&@gmail.com>
2023-02-17 16:25:31 +0200

    Delete comment

--- HelloWorldServlet/src/main/java/com/srccodes/example/HelloWorld.java
+++ HelloWorldServlet/src/main/java/com/srccodes/example/HelloWorld.java
@@ -8,55 +8,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
+
  * Servlet implementation class HelloWorld
- */
+ 
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
+      
      * @see HttpServlet#HttpServlet()
-     */
     public HelloWorld() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.setContentType("text/html");
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>Hello World!</h1>");
 	}
 
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>Hello World!</h1>");
 	}
 
-	/**
+	
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>Hello World!</h1>");
 	}
 
-	/**
+	
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>Hello World!</h1>");
 	}
